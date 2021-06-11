18663c7661b5510956dae8d36aeb705f10e82501
Ky <kyrill@gobber.ch>
2021-06-11 23:58:05 +0200

    PureComponent on Player

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -18,7 +18,6 @@ class AddPlayerForm extends Component {
     }
 
     render() {
-        console.log(this.state.value);
         return (
             <form onSubmit={this.handleSubmit}>
                 <input
